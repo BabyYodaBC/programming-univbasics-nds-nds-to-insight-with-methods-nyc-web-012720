fc9666709c6438d4d979dbdf546aea99aba480dd
ricky wahba <rjwahba@gmail.com>
2020-01-09 02:49:53 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -6,6 +6,9 @@ require 'pp'
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
 def gross_for_director(director_data)
+  row_index = 0
+  gross_total = 0
+  while row_index < nds[row_index][:movies]
 
 
 end
